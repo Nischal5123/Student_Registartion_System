325ce864a41aa47c5aa1f2b06e692ea8382c2b40
Nischal Aryal <nischal5123.github.io>
2019-04-22 20:11:39 -0500

    course registration validation
    check correct course name entered
    check cannot change for un-registered course

--- course_edited.php
+++ course_edited.php
@@ -12,11 +12,27 @@ mysqli_select_db($connect,"webexpress");
 $name=$_POST['name'];
 $course=$_POST['course'];
 $new=$_POST['new'];
-
+//check if un available course is being tried to replace 
+$q2="SELECT name FROM course WHERE name='$new'";                  
+$r2=mysqli_query($connect,$q2) or die(mysqli_error($connect));
+$reg2=mysqli_fetch_assoc($r2);
+if(mysqli_num_rows($r2) == 0)
+{echo "<a href='edit_course.php'>Back</a><br/>Course doesnot exist $name";
+exit();
+}
+//check if the Course to change is relevent
+$q3="SELECT cname FROM regis WHERE cname='$course'";                  
+$r3=mysqli_query($connect,$q3) or die(mysqli_error($connect));
+$reg3=mysqli_fetch_assoc($r3);
+if(mysqli_num_rows($r3) == 0)
+{echo "<a href='edit_course.php'>Back</a><br/>Entered wrong course to be changed $name";
+exit();
+}
+else{
 $query = "update regis SET cname = '$new' WHERE uname = '$name' AND cname = '$course'";
 $results=mysqli_query($connect,$query) or die(mysqli_error());
 echo"<a href='edit_course.php'>Back</a><br/>COURSE CHANGED SUCESSFULLY";
-
+}
 ?>
 
 <html>
